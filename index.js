3cbf3ed311eb6221932a49915c7cee6b6833f9f6
ayumitanaka <tanakayumi13@gmail.com>
2020-07-18 22:58:15 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// gsap.registerPlugin(ScrollTrigger);
+
+// gsap.to(".panel:not(:last-child)", {
+//   yPercent: -80, 
+//   ease: "none",
+//   stagger: 0.5,
+//   opacity: 0,
+//   scrollTrigger: {
+//     trigger: "#body",
+//     start: "top top",
+//     end: "+=300%",
+//     scrub: true,
+//     pin: true
+//   }
+// });
+
+// gsap.set(".panel", {zIndex: (i, target, targets) => targets.length - i});
