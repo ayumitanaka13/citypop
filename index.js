5279491fe598d6d6cba9e41c25d5de1f968fb493
ayumitanaka <tanakayumi13@gmail.com>
2020-07-21 15:03:40 -0700

    add favicon

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-// gsap.registerPlugin(ScrollTrigger);
+let tl = gsap.timeline({delay: 1});
+    
+tl.from("#title", {
+    x: -400,
+    opacity: 0,
+    ease: "power4",
+    duration: 3,
+},0.5)
+.from("#picture", {
+    x: 400,
+    opacity: 0,
+    ease: "power4",
+    duration: 3,
+},0.5);
 
-// gsap.to(".panel:not(:last-child)", {
-//   yPercent: -80, 
-//   ease: "none",
-//   stagger: 0.5,
-//   opacity: 0,
-//   scrollTrigger: {
-//     trigger: "#body",
-//     start: "top top",
-//     end: "+=300%",
-//     scrub: true,
-//     pin: true
-//   }
-// });
+// CSSPlugin.defaultTransformPerspective = 400; 
 
-// gsap.set(".panel", {zIndex: (i, target, targets) => targets.length - i});
+// gsap.to("#picture", {duration: 3,  x: 0, y: 0, scale: 1.1, skewX: 25, rotation: 5});
+// gsap.to(".orange", {duration: 3, rotationY: 360});
+// gsap.to(".grey", {duration: 3, x: 100, y: 100, scale: 2, skewX: 45, rotation: 180});
