089af366d39d1bbe45e46c1006be4ba1cf85d78d
ayumitanaka <tanakayumi13@gmail.com>
2020-07-24 20:17:35 -0700

    add page

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// gsap.registerPlugin(ScrollTrigger);
+
+// let sections = gsap.utils.toArray(".panel");
+
+// gsap.to(sections, {
+//   xPercent: -100 * (sections.length - 1),
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: ".containerbox",
+//     pin: true,
+//     scrub: 1,
+//     snap: 1 / (sections.length - 1),
+//     // base vertical scrolling on how wide the container is so it feels more natural.
+//     end: () => "+=" + document.querySelector(".containerbox").offsetWidth
+//   }
+// });
+
 let tl = gsap.timeline({delay: 1});
     
 tl.from("#title", {
